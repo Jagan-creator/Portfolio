94f196937e16a7ea27c558145e02cb90a5fcd057
Joel Hagan <joel.hagan.8@gmail.com>
2023-07-18 15:15:37 -0600

    add baseline links for navbar categories

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -11,6 +12,29 @@ const Navbar = () => {
           width="150"
           height="75"
         />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/#about">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/#skills">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/#projects">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/#contact">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+        </div>
       </div>
     </div>
   );
