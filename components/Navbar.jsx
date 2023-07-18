b83e7b9774c24798040dda48a37a30e31393206e
Joel Hagan <joel.hagan.8@gmail.com>
2023-07-18 15:06:51 -0600

    begin implementation of NavBar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/../public/assets/joel-logo.png"
+          alt="/"
+          width="150"
+          height="75"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Head from "next/head"
+import Navbar from "@/components/Navbar"
+
 export default function Home() {
   return (
-    <main>
-
-    </main>
+    <div>
+      <Head>
+        <title>Joel | Full-Stack Developer</title>
+        <meta name="description" content="content description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
   )
 }
