0fdc8a2d3ab15ae26f95357ac080f7d4ab187d34
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-12 16:56:22 -0600

    create baseline for Main component

---
+++ components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            BUILDING AWESOME THINGS
+          </p>
+          <h1>Hi, I'm Joel</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
