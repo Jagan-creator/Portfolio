5c6f9b6dcdfb877455af23de19e023d525bd07fa
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-13 16:19:37 -0600

    move project to it's own component

---
+++ components/ProjectItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+import mimikyuImg from "../public/assets/projects/mimikyus-day-off-preview.png";
+import Link from "next/link";
+
+const ProjectItem = () => {
+  return (
+    <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-3 group hover:bg-gradient-to-r from-[#f0a10e] to-[#ffd771]">
+      <Image
+        className="rounded-xl group-hover:opacity-70"
+        src={mimikyuImg}
+        alt="/"
+      />
+      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <h3 className="text-2xl text-white tracking-wider text-center">
+          Mimikyu's Day Off
+        </h3>
+        <p className="pb-4 pt-2 text-white text-center">Next.js & Prisma</p>
+        <Link href="/">
+          <p className="text-center py-3 rounded-lg bg-white text-gray-400 font-bold text-lg cursor-pointer">
+            More Info
+          </p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
