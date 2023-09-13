91e6c35c09b0c4dbe4beb2a6a94ff94d8d5c4229
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-13 13:48:24 -0600

    finish out main component landing

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 
 const Main = () => {
   return (
@@ -6,9 +9,33 @@ const Main = () => {
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">
-            BUILDING AWESOME THINGS
+            CODE CREATE DELIVER
           </p>
-          <h1>Hi, I'm Joel</h1>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#f0a10e]">Joel</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">A Full-Stack Web Developer</h1>
+          <p className="py-4 text-gray-600 max-width-[70%] m-auto">
+            As an inspired full-stack web developer I aim to seamlessly blend
+            creativity and technical expertise. It's about crafting engaging
+            user interfaces and robust back-end systems. I thrive on innovation,
+            adapting to new technologies, and delivering solutions that empower
+            users and businesses in the digital world.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <BsFillPersonLinesFill />
+            </div>
+          </div>
         </div>
       </div>
     </div>
