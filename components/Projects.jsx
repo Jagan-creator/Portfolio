e161d207898bdb13b2056b29e672751b0f5c4cc8
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-13 17:36:27 -0600

    implement ProjectItem into Projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
+import mimikyuImg from "../public/assets/projects/mimikyu-day-off-preview.png";
+import jungleImg from "../public/assets/projects/jungle-preview.jpeg";
+import schedulerImg from "../public/assets/projects/scheduler-preview.png";
+import tweeterImg from "../public/assets/projects/tweeter-preview.png";
 
 function Projects() {
   return (
@@ -10,7 +14,30 @@ function Projects() {
         </p>
         <h2 className="py-4">Things I Have Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <ProjectItem />
+          <ProjectItem
+            title="Mimikyu's Day Off"
+            backgroundImg={mimikyuImg}
+            framework="Next.js / Prisma"
+            projectUrl="/mimikyu"
+          />
+          <ProjectItem
+            title="Jungle"
+            backgroundImg={jungleImg}
+            framework="Ruby / Rails"
+            projectUrl="/jungle"
+          />
+          <ProjectItem
+            title="Scheduler"
+            backgroundImg={schedulerImg}
+            framework="JavaScript / API / Testing"
+            projectUrl="/schedule"
+          />
+          <ProjectItem
+            title="Tweeter"
+            backgroundImg={tweeterImg}
+            framework="Javascript / CSS / jQuery"
+            projectUrl="/tweeter"
+          />
         </div>
       </div>
     </div>
