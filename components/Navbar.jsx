58abac970a7fd146408d9c38cf72f262d1287291
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-14 12:19:51 -0600

    implement shadow on nav and smooth scrolling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
@@ -7,37 +7,72 @@ import { BsFillPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
+  const [shadow, setShadow] = useState(false);
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
+
   return (
-    <div className="fixed w-full h-20 shadow-xl z-[100]">
+    <div
+      className={
+        shadow
+          ? "fixed w-full h-20 shadow-xl z-[100]"
+          : "fixed w-full h-20 z-[100]"
+      }
+    >
       <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-        <Image
-          src="/../public/assets/joel-logo.png"
-          alt="/"
-          width="150"
-          height="75"
-        />
+        <Link href="/">
+          <Image
+            src="/../public/assets/joel-logo.png"
+            alt="/"
+            width="150"
+            height="75"
+          />
+        </Link>
         <div>
           <ul className="hidden md:flex">
-            <Link href="/">
+            <Link
+              scroll={false}
+              href="/"
+            >
               <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
             </Link>
-            <Link href="/#about">
+            <Link
+              scroll={false}
+              href="/#about"
+            >
               <li className="ml-10 text-sm uppercase hover:border-b">About</li>
             </Link>
-            <Link href="/#skills">
+            <Link
+              scroll={false}
+              href="/#skills"
+            >
               <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
             </Link>
-            <Link href="/#projects">
+            <Link
+              scroll={false}
+              href="/#projects"
+            >
               <li className="ml-10 text-sm uppercase hover:border-b">
                 Projects
               </li>
             </Link>
-            <Link href="/#contact">
+            <Link
+              scroll={false}
+              href="/#contact"
+            >
               <li className="ml-10 text-sm uppercase hover:border-b">
                 Contact
               </li>
