6bd249dd877e2143bbaa43bc3a3b330da1d58ec4
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-14 12:20:38 -0600

    add id's for appropriate pages

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,7 +7,10 @@ import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 function Contact() {
   return (
-    <div className="w-full lg:h-screen">
+    <div
+      id="contact"
+      className="w-full lg:h-screen"
+    >
       <div className="max-w-[1240px] m-auto px-2 py-16 w-full">
         <p className="text-xl tracking-widest uppercase text-[#f0a10e]">
           Contact
