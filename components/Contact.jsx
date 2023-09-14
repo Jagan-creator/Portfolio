7c2ea689bc4362cdf0d62c4166d33d2b332ed02c
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-14 13:21:24 -0600

    fix to top of page button smooth scrolling

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -103,7 +103,10 @@ function Contact() {
           </div>
         </div>
         <div className="flex justify-center py-12">
-          <Link href="/">
+          <Link
+            scroll={false}
+            href="/#home"
+          >
             <div className="rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300">
               <HiOutlineChevronDoubleUp
                 className="text-[#f0a10e]"
