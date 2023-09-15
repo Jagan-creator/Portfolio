9b0e55caae806d3e09f558cb0943f2f7edf56b05
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-14 18:05:39 -0600

    update shadow on CODE buttons for project pages

--- pages/scheduler.jsx
+++ pages/scheduler.jsx
@@ -54,7 +54,9 @@ const scheduler = () => {
             vitae tempus quam pellentesque nec. Sed euismod nisi porta lorem
             mollis.
           </p>
-          <button className="px-8 py-2 mt-4">Code</button>
+          <button className="px-8 py-2 mt-4 mr-8 cursor-pointer hover:scale-110 ease-in duration-300 shadow-md shadow-gray-400">
+            Code
+          </button>
         </div>
         <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
           <div className="p-2">
