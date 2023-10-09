93b862a471e1d1fab429d79cc301c64287281578
Joel Hagan <joel.hagan.8@gmail.com>
2023-10-09 15:51:35 -0600

    update scheduler description

--- pages/scheduler.jsx
+++ pages/scheduler.jsx
@@ -27,29 +27,27 @@ const scheduler = () => {
         <div className="col-span-4">
           <p>Project</p>
           <h2>Overview</h2>
-          <p className="py-4">
-            Interview Scheduler stands as a feature-rich single-page web
-            application, thoughtfully crafted with React as its cornerstone
-            technology. This platform simplifies the appointment booking process
-            by enabling users to seamlessly schedule appointments with
-            interviewers, taking advantage of a dynamic schedule that reflects
-            real-time availability. Notably, this app doesn't stop at the
-            basics; it goes the extra mile to deliver a polished and
-            user-centric experience. The application's core functionality
-            encompasses an intuitive interface where users can easily spot
-            available days and slots, make, and even cancel appointments.
-            Moreover, it offers advanced features like the ability to edit
-            appointment details, ensuring flexibility and adaptability to
-            changing circumstances. Crucially, Interview Scheduler implements
-            confirmation checks to prevent accidental deletions and error
-            handlers to ensure data integrity, requiring essential information
-            such as student names and chosen interviewers. In summary, Interview
-            Scheduler is not just a functional appointment management tool; it's
-            a showcase of modern web development powered by React, enriched by
-            robust testing practices, and designed to provide a seamless and
-            user-friendly experience for scheduling interviews with ease and
-            confidence.
-          </p>
+          <ul className="py-4">
+            <p className="text-gray-600 py-2 flex items-center">
+              <RiRadioButtonFill className="p-1" />
+              Easy to use scheduler application that can book, cancel, and edit
+              appointments with mentors
+            </p>
+            <p className="text-gray-600 py-2 flex items-center">
+              <RiRadioButtonFill className="p-1" />
+              Various error and warning notices are integrated for a better user
+              experience
+            </p>
+            <p className="text-gray-600 py-2 flex items-center">
+              <RiRadioButtonFill className="p-1" />
+              Simplistic navigation that shows students what areas they can book
+              an appointment
+            </p>
+            <p className="text-gray-600 py-2 flex items-center">
+              <RiRadioButtonFill className="p-1" />
+              Live updating of notifications and appointment status
+            </p>
+          </ul>
           <a
             href="https://github.com/Jagan-creator/scheduler"
             target="_blank"
